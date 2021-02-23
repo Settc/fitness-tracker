f9eb8a9c5e909161cee82fb31b8eed9144e8b628
Thomas Whittle <tewhittle@gmail.com>
2021-02-23 10:32:43 -0500

    Add html routes to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,31 @@
+require("dotenv").config()
 const express = require("express")
-
 const mongoose = require("mongoose")
 
+const path = require("path")
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.urlencoded( {extended: true} ))
+app.use(express.json())
+app.use(express.static("public"))
+
+mongoose.connect(process.env.MONGO_URI)
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/index.html"))
+  })
+  
+  app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+  });
+  
+  app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/stats.html"))
+  })
+
+  app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`)
+})
